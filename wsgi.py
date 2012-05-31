650dff26d3f4f1f39485faa99fab0b5b2d6818c3
Mark Brough <mark@mark-Aspire-5755G.(none)>
2012-05-31 16:37:41 +0200

    Updated documentation

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,9 @@
 import sys
-sys.path.append('/home/dotcloud/current')
+import logging, sys
+import os
+logging.basicConfig(stream=sys.stderr)
+activate_this = 'ABS_PATH_TO_YOUR_PYENV_IN_MODELEDITOR_FOLDER/CSV-IATI-Converter.modeleditor/pyenv/bin/activate_this.py'
+execfile(activate_this, dict(__file__=activate_this))
+sys.path.insert(0, 'ABS_PATH_TO_MODELEDITOR_FOLDER/CSV-IATI-Converter.modeleditor')
 from csviatimodeleditor import app as application
+
